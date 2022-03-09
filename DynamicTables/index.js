b1bd1f8b9254169a8d03f2bdc11f5e4bbfad1642
Borovikov Kirill <bks1105@gmail.com>
2022-03-09 23:13:53 +0300

    Apply reduce

--- DynamicTables/index.js
+++ DynamicTables/index.js
@@ -8,23 +8,18 @@ const divs = {}
 const createContainers = () => 
 {
     const positionsArr = getGridsParams()
-    let topDivs = ''
-    let bottomPills = ''
-    
-    positionsArr.forEach(p => {
-        topDivs += `<div class="col-sm-4 tableFixHead" id="${p}-div" data-position="${p}"></div>`
-        bottomPills += `<li class="nav-item">
-                            <a  class="nav-link ${positionsArr.indexOf(p) === 0 ? 'active': ''}" 
-                            id="tab-${p}" 
+    debugger
+    const topDivs = positionsArr.reduce((init, result) => init + `<div class="col-sm-4 tableFixHead" id="${result}-div" data-position="${result}"></div>`, '')
+    const bottomPills = positionsArr.reduce((init, result, index) => init +
+                    `<li class="nav-item">
+                        <a  class="nav-link ${index === 0 ? 'active': ''}" 
+                            id="tab-${result}" 
                             data-toggle="pill" 
                             href="javascript:void(0)" 
-                            data-link="${p}">${p}</a>
-                        </li>`
-    })
+                            data-link="${result}">${result}</a>
+                    </li>`, '')    
     
-    topContainer.innerHTML = ''
     topContainer.innerHTML = topDivs
-    pillsContainer.innerHTML = ''
     pillsContainer.innerHTML = bottomPills
     const tabs = {}
     positionsArr.forEach(p => {
